94f0416686daeda234bbb4e639c40ce9330bbc5a
David Bernhard <davidbernhard@live.com>
2019-02-26 15:52:14 +0000

    add pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const About = () => (
+  <div>
+    <div style={{ color: `teal` }}>
+      <h1>About Gatsby</h1>
+      <p>Such wow. Very React.</p>
+    </div>
+  </div>
+)
+
+export default About
