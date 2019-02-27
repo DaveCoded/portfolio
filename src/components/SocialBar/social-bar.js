b1d8def0c6fb2e1d3b6d037a705e3bdbf21d856f
David Bernhard <davidbernhard@live.com>
2019-02-27 13:53:30 +0000

    create SocialBar component

---
+++ src/components/SocialBar/social-bar.js
@@ -0,0 +1,6 @@
+import React from "react"
+import styles from "./social-bar.module.scss"
+
+const SocialBar = () => <div className={styles.socialBar} />
+
+export default SocialBar
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from "react"
 import styles from "./index.module.scss"
 
 import Layout from "../components/layout"
+import SocialBar from "../components/SocialBar/social-bar"
 
 const HomePage = () => (
   <Layout>
     <section className={styles.background}>
       <div className={styles.polygon} />
+      <SocialBar />
       <h1>Hi. My name is Dave.</h1>
     </section>
   </Layout>
