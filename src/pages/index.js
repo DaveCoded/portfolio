620dffc999fcc16826fc7b9d7c436dafbf85fecc
David Bernhard <davidbernhard@live.com>
2019-02-27 13:18:27 +0000

    pink polygon & nav items

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Layout from "../components/layout"
 const HomePage = () => (
   <Layout>
     <section className={styles.background}>
+      <div className={styles.polygon} />
       <h1>Hi. My name is Dave.</h1>
     </section>
   </Layout>
