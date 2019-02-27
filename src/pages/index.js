3b2890a09abd7eb5a77bfd5f9f48d2c5ef054d4f
David Bernhard <davidbernhard@live.com>
2019-02-27 20:27:12 +0000

    refactor url links

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,40 +9,31 @@ import linkedin from "../assets/linkedin-button.svg"
 import twitter from "../assets/twitter-button.svg"
 import coderlist from "../assets/coderlist-button.svg"
 
-const HomePage = () => (
-  <Layout>
-    <section className={styles.background}>
-      <div className={styles.polygon}>
-        <h1 className={styles.heading}>
-          Hi. My name is Dave. <br /> Welcome to my portfolio.
-        </h1>
-        <hr />
-      </div>
-      <SocialBar />
-      <div className={styles.socialdiv}>
-        <SocialButton
-          href="https://github.com/DaveCoded"
-          src={github}
-          alt="Github logo"
-        />
-        <SocialButton
-          href="https://www.linkedin.com/in/dave-bernhard-93b0b876/"
-          src={linkedin}
-          alt="LinkedIn logo"
-        />
-        <SocialButton
-          href="https://twitter.com/earthwormdave"
-          src={twitter}
-          alt="Twitter logo"
-        />
-        <SocialButton
-          href="https://www.coderlist.io/profile/coderlist423"
-          src={coderlist}
-          alt="CoderList logo"
-        />
-      </div>
-    </section>
-  </Layout>
-)
+const HomePage = () => {
+  const githubURL = "https://github.com/DaveCoded"
+  const linkedinURL = "https://www.linkedin.com/in/dave-bernhard-93b0b876/"
+  const twitterURL = "https://twitter.com/earthwormdave"
+  const coderlistURL = "https://www.coderlist.io/profile/coderlist423"
+
+  return (
+    <Layout>
+      <section className={styles.background}>
+        <div className={styles.polygon}>
+          <h1 className={styles.heading}>
+            Hi. My name is Dave. <br /> Welcome to my portfolio.
+          </h1>
+          <hr />
+        </div>
+        <SocialBar />
+        <div className={styles.socialdiv}>
+          <SocialButton href={githubURL} src={github} alt="Github" />
+          <SocialButton href={linkedinURL} src={linkedin} alt="LinkedIn" />
+          <SocialButton href={twitterURL} src={twitter} alt="Twitter" />
+          <SocialButton href={coderlistURL} src={coderlist} alt="CoderList" />
+        </div>
+      </section>
+    </Layout>
+  )
+}
 
 export default HomePage
