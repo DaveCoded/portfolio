2af27350749201910cbf3a3a0d15eb41add2cd68
David Bernhard <davidbernhard@live.com>
2019-03-01 01:51:31 +0000

    padding and margins

--- src/components/HomeButton/homebutton.js
+++ src/components/HomeButton/homebutton.js
@@ -4,7 +4,12 @@ import house from "../../assets/svg/home-blue.svg"
 const HomeButton = () => (
   <img
     src={house}
-    style={{ width: "5.2rem", display: "inline-block" }}
+    style={{
+      width: "5.2rem",
+      display: "inline-block",
+      marginLeft: "3.8rem",
+      marginTop: "1rem",
+    }}
     alt="Home link"
   />
 )
