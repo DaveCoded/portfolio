296a0465f78b404140b8e46e4aadbe186f6dad17
David Bernhard <davidbernhard@live.com>
2019-03-02 17:56:04 +0000

    footer copyright

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,21 +5,29 @@ import styles from "./layout.module.scss"
 import ListLink from "./ListLink/ListLink"
 import HomeButton from "../components/HomeButton/homebutton"
 
-export default ({ children }) => (
-  <div className={styles.layout}>
-    <nav>
-      <Link to="/">
-        <HomeButton />
-      </Link>
-      <ul>
-        <ListLink to="/about/">about</ListLink>
-        <ListLink to="/projects/">projects</ListLink>
-        <ListLink to="/blog/">blog</ListLink>
-        <ListLink to="/cv/">cv</ListLink>
-        <ListLink to="/contact/">contact</ListLink>
-      </ul>
-    </nav>
-    {children}
-    <footer />
-  </div>
-)
+const Layout = ({ children }) => {
+  let year = new Date().getFullYear()
+
+  return (
+    <div className={styles.layout}>
+      <nav>
+        <Link to="/">
+          <HomeButton />
+        </Link>
+        <ul>
+          <ListLink to="/about/">about</ListLink>
+          <ListLink to="/projects/">projects</ListLink>
+          <ListLink to="/blog/">blog</ListLink>
+          <ListLink to="/cv/">cv</ListLink>
+          <ListLink to="/contact/">contact</ListLink>
+        </ul>
+      </nav>
+      {children}
+      <footer>
+        <p>&copy;{year} David Bernhard</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
