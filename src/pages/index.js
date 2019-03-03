7355bc1793af8849e873d4c743919e522a53b8e6
David Bernhard <davidbernhard@live.com>
2019-03-03 18:17:56 +0000

    animate only on first page load

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import styles from "./index.module.scss"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
@@ -11,70 +11,96 @@ import linkedin from "../assets/svg/linkedin-button.svg"
 import twitter from "../assets/svg/twitter-button.svg"
 import coderlist from "../assets/svg/coderlist-button.svg"
 
-const HomePage = ({ data }) => {
-  const githubURL = "https://github.com/DaveCoded"
-  const linkedinURL = "https://www.linkedin.com/in/dave-bernhard-93b0b876/"
-  const twitterURL = "https://twitter.com/earthwormdave"
-  const coderlistURL = "https://www.coderlist.io/profile/coderlist423"
+class HomePage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-  return (
-    <Layout>
-      <section className={styles.topBackground}>
-        <div className={styles.polygon}>
-          <h1 className={styles.heading}>
-            <span className={styles.firstBit}>Hi. My name is Dave.</span>
-            <span className={styles.secondBit}>Welcome to my portfolio.</span>
-          </h1>
-          <hr />
-        </div>
-        <SocialBar />
-        <div className={styles.socialdiv}>
-          <SocialButton href={githubURL} src={github} alt="Github" />
-          <SocialButton href={linkedinURL} src={linkedin} alt="LinkedIn" />
-          <SocialButton href={twitterURL} src={twitter} alt="Twitter" />
-          <SocialButton href={coderlistURL} src={coderlist} alt="CoderList" />
-        </div>
-        <div className={styles.description}>
+  render() {
+    const githubURL = "https://github.com/DaveCoded"
+    const linkedinURL = "https://www.linkedin.com/in/dave-bernhard-93b0b876/"
+    const twitterURL = "https://twitter.com/earthwormdave"
+    const coderlistURL = "https://www.coderlist.io/profile/coderlist423"
+
+    return (
+      <Layout>
+        <section className={styles.topBackground}>
+          <div className={styles.polygon}>
+            <h1 className={styles.heading}>
+              <span className={this.state.firstSpanClass}>
+                Hi. My name is Dave.
+              </span>
+              <span className={this.state.secondSpanClass}>
+                Welcome to my portfolio.
+              </span>
+            </h1>
+            <hr />
+          </div>
+          <SocialBar />
+          <div className={styles.socialdiv}>
+            <SocialButton href={githubURL} src={github} alt="Github" />
+            <SocialButton href={linkedinURL} src={linkedin} alt="LinkedIn" />
+            <SocialButton href={twitterURL} src={twitter} alt="Twitter" />
+            <SocialButton href={coderlistURL} src={coderlist} alt="CoderList" />
+          </div>
+          <div className={styles.description}>
+            <p>
+              I am a front end developer with a <br /> passion for JavaScript,
+              CSS and creating <br /> beautiful experiences that are accessible
+              to all
+            </p>
+          </div>
+        </section>
+        <section className={styles.sectionTwo}>
+          <div className={styles.eenyText}>
+            <p>
+              In August 2018, I started{" "}
+              <span className={styles.eenySpan}>
+                Eeny <br /> Meeny Creative
+              </span>{" "}
+              with a friend. We <br /> wanted to learn on the job, by <br />{" "}
+              founding our own company...
+            </p>
+          </div>
+          <div className={styles.eenyDiv}>
+            <Img fluid={this.props.data.eenyLogo.childImageSharp.fluid} />
+          </div>
+        </section>
+        <section className={styles.sectionThree}>
+          <Img fluid={this.props.data.laptop.childImageSharp.fluid} />
           <p>
-            I am a front end developer with a <br /> passion for JavaScript, CSS
-            and creating <br /> beautiful experiences that are accessible to all
+            Now I want to join a team of developers <br />
+            to <span>learn</span> exciting new technologies and methods...
           </p>
-        </div>
-      </section>
-      <section className={styles.sectionTwo}>
-        <div className={styles.eenyText}>
-          <p>
-            In August 2018, I started{" "}
-            <span className={styles.eenySpan}>
-              Eeny <br /> Meeny Creative
-            </span>{" "}
-            with a friend. We <br /> wanted to learn on the job, by <br />{" "}
-            founding our own company...
+        </section>
+        <section className={styles.sectionFour}>
+          <Img fluid={this.props.data.mountainJump.childImageSharp.fluid} />
+          <p className={styles.paraOne}>
+            To <span>collaborate</span> on something bigger than <br /> the sum
+            of its parts...
           </p>
-        </div>
-        <div className={styles.eenyDiv}>
-          <Img fluid={data.eenyLogo.childImageSharp.fluid} />
-        </div>
-      </section>
-      <section className={styles.sectionThree}>
-        <Img fluid={data.laptop.childImageSharp.fluid} />
-        <p>
-          Now I want to join a team of developers <br />
-          to <span>learn</span> exciting new technologies and methods...
-        </p>
-      </section>
-      <section className={styles.sectionFour}>
-        <Img fluid={data.mountainJump.childImageSharp.fluid} />
-        <p className={styles.paraOne}>
-          To <span>collaborate</span> on something bigger than <br /> the sum of
-          its parts...
-        </p>
-        <p className={styles.paraTwo}>
-          and make the web better for <span>everyone</span>
-        </p>
-      </section>
-    </Layout>
-  )
+          <p className={styles.paraTwo}>
+            and make the web better for <span>everyone</span>
+          </p>
+        </section>
+      </Layout>
+    )
+  }
+  componentDidMount() {
+    if (window.sessionStorage.getItem("firstLoadDone") === null) {
+      this.setState({
+        firstSpanClass: styles.firstBitAnimated,
+        secondSpanClass: styles.secondBitAnimated,
+      })
+      window.sessionStorage.setItem("firstLoadDone", 1)
+    } else {
+      this.setState({
+        firstSpanClass: styles.firstBit,
+        secondSpanClass: styles.secondBit,
+      })
+    }
+  }
 }
 
 export default HomePage
