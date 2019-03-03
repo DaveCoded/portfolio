8d8d8c78f4d35f9920a928b50a97e5dbb3ab322a
David Bernhard <davidbernhard@live.com>
2019-03-03 02:32:35 +0000

    active link styles

--- src/components/ListLink/ListLink.js
+++ src/components/ListLink/ListLink.js
@@ -5,7 +5,16 @@ import styles from "./ListLink.module.scss"
 
 const ListLink = props => (
   <li className={styles.listLink}>
-    <Link to={props.to}>{props.children}</Link>
+    <Link
+      to={props.to}
+      activeStyle={{
+        borderBottom: "3px solid #1cefef",
+        paddingBottom: "5px",
+        color: "#e93b74",
+      }}
+    >
+      {props.children}
+    </Link>
   </li>
 )
 
