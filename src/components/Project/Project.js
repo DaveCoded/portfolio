4d862d879b1a7986d01972513c9479d0f26c8e48
David Bernhard <davidbernhard@live.com>
2019-03-07 19:18:37 +0000

    simplify Project component

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,28 +2,14 @@ import React from "react"
 
 import Link from "gatsby-link"
 import Img from "gatsby-image"
+// import styles from "./Project.module.scss"
 
 const Project = ({ node }) => (
-  <article
-    style={{
-      marginBottom: "1.5rem",
-      padding: "1.5rem",
-      border: "1px solid #ccc",
-    }}
-  >
-    <h3>
-      <Link to={`projects/${node.slug}`}>{node.title}</Link>
-    </h3>
-    <p>{node.link}</p>
+  <Link to={`projects/${node.slug}`}>
     <div>
-      <div>
-        <Img resolutions={node.featuredImage.resolutions} />
-      </div>
-      <div>
-        <p>{node.content.childMarkdownRemark.excerpt}</p>
-      </div>
+      <Img resolutions={node.featuredImage.resolutions} />
     </div>
-  </article>
+  </Link>
 )
 
 export default Project
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,44 +5,6 @@ import Layout from "../components/layout"
 import styles from "./projects.module.scss"
 import Project from "../components/Project/Project"
 
-/*
-const Project = ({ node }) => {
-  return (
-    <article
-      style={{
-        marginBottom: "1.5rem",
-        padding: "1.5rem",
-        border: "1px solid #ccc",
-      }}
-    >
-      <h3>
-        <Link to={`projects/${node.slug}`}>{node.title}</Link>
-      </h3>
-      <p>{node.link}</p>
-      <div>
-        <div>
-          <Img resolutions={node.featuredImage.resolutions} />
-        </div>
-        <div>
-          <p>{node.content.childMarkdownRemark.excerpt}</p>
-        </div>
-      </div>
-    </article>
-  )
-}
-
-const ProjectsPage = props => {
-  return (
-    <Layout>
-      <main className={styles.main}>
-        {props.data.allContentfulProject.edges.map(edge => (
-          <Project key={edge.node.id} node={edge.node} />
-        ))}
-      </main>
-    </Layout>
-  )
-}*/
-
 const ProjectsPage = props => {
   return (
     <Layout>
