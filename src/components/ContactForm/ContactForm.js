e954b81cf4c2754bd3a0db2ea60d8d54e9dbb816
David Bernhard <davidbernhard@live.com>
2019-03-18 14:52:33 +0000

    change to class-based component

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,31 +1,40 @@
-import React from "react"
+import React, { Component } from "react"
 import styles from "./ContactForm.module.scss"
 
-const ContactForm = () => (
-  <section className={styles.formWrapper}>
-    <form name="contact" method="POST" data-netlify="true" action="/contact/">
-      <div className={styles.formInput}>
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
-      </div>
-      <div className={styles.formInput}>
-        <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" />
-      </div>
-      <div>
-        <label htmlFor="message">Message</label>
-        <textarea name="message" id="message" rows="6" />
-      </div>
-      <ul>
-        <li>
-          <input type="submit" value="Send Message" />
-        </li>
-        <li>
-          <input type="reset" value="Clear" />
-        </li>
-      </ul>
-    </form>
-  </section>
-)
+class ContactForm extends Component {
+  render() {
+    return (
+      <section className={styles.formWrapper}>
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          action="/contact/"
+        >
+          <div className={styles.formInput}>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div className={styles.formInput}>
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" />
+          </div>
+          <div>
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message" rows="6" />
+          </div>
+          <ul>
+            <li>
+              <input type="submit" value="Send Message" />
+            </li>
+            <li>
+              <input type="reset" value="Clear" />
+            </li>
+          </ul>
+        </form>
+      </section>
+    )
+  }
+}
 
 export default ContactForm
