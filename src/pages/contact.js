1461d8ce04ce97baaabc9ba40d305259413e4ccf
David Bernhard <davidbernhard@live.com>
2019-03-19 13:21:13 +0000

    stateful form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,30 +1,86 @@
 import React from "react"
 import Layout from "../components/layout"
+import { navigateTo } from "gatsby-link"
 
-const Contact = () => (
-  <Layout>
-    <div style={{ margin: "10rem" }}>
-      <h1
-        style={{
-          marginBottom: "6rem",
-          fontSize: "4rem",
-          fontFamily: `"Pompiere", "Neutra Text Light", sansSerif`,
-        }}
-      >
-        Get in touch
-      </h1>
-      <form
-        name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="bot-field" />
-        <input name="name" placeholder="Your Name" type="text" />
-        <button>Send</button>
-      </form>
-    </div>
-  </Layout>
-)
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
 
-export default Contact
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state,
+      }),
+    })
+      .then(() => navigateTo(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
+
+  render() {
+    return (
+      <Layout>
+        <div>
+          <h1>Contact</h1>
+          <form
+            name="contact"
+            method="post"
+            action="/thanks/"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={this.handleSubmit}
+          >
+            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+            <input type="hidden" name="form-name" value="contact" />
+            <p hidden>
+              <label>
+                Don’t fill this out:{" "}
+                <input name="bot-field" onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your name:
+                <br />
+                <input type="text" name="name" onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your email:
+                <br />
+                <input type="email" name="email" onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message:
+                <br />
+                <textarea name="message" onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+        </div>
+      </Layout>
+    )
+  }
+}
