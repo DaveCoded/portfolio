2b20a13f17d678dc62792a7fe90137aa16f06d61
David Bernhard <davidbernhard@live.com>
2019-03-19 12:38:08 +0000

    add /success route

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -7,6 +7,7 @@ class ContactForm extends Component {
       <form
         name="contact"
         method="post"
+        action="/success"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
       >
---
+++ src/pages/success.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const success = () => {
+  return (
+    <Layout>
+      <div>
+        <h1 style={{ marginTop: "15rem", textAlign: "center" }}>
+          Thank you for submitting!
+        </h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default success
