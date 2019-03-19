3a412fe0e895897043871cd242b35b86347858e9
David Bernhard <davidbernhard@live.com>
2019-03-19 12:51:57 +0000

    remove bot-field

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,16 +1,9 @@
 import React, { Component } from "react"
-import styles from "./ContactForm.module.scss"
 
 class ContactForm extends Component {
   render() {
     return (
-      <form
-        name="contact"
-        method="post"
-        action="/success"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
+      <form name="contact" method="post" action="/success/" data-netlify="true">
         <input name="name" placeholder="Your Name" type="text" />
         <button>Send</button>
       </form>
