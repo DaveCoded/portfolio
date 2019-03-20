fc6d3137999e276e2da475f484f3186b0ab83a3a
David Bernhard <davidbernhard@live.com>
2019-03-20 15:53:29 +0000

    position background and overlay

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -42,53 +42,59 @@ export default class Contact extends React.Component {
       <Layout>
         <BackgroundSection>
           <div className={styles.formContainer}>
-            <h1>Get in touch</h1>
-            <form
-              name="contact"
-              method="post"
-              action="/thanks/"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-              onSubmit={this.handleSubmit}
-            >
-              {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-              <input type="hidden" name="form-name" value="contact" />
-              <p hidden>
-                <label>
-                  Don’t fill this out:{" "}
-                  <input name="bot-field" onChange={this.handleChange} />
-                </label>
-              </p>
-              <p>
-                <label>
-                  Your name:
-                  <br />
-                  <input type="text" name="name" onChange={this.handleChange} />
-                </label>
-              </p>
-              <p>
-                <label>
-                  Your email:
-                  <br />
-                  <input
-                    type="email"
-                    name="email"
-                    onChange={this.handleChange}
-                  />
-                </label>
-              </p>
-              <p>
-                <label>
-                  Message:
-                  <br />
-                  <textarea name="message" onChange={this.handleChange} />
-                </label>
-              </p>
-              <p>
-                <button type="submit">Send</button>
-              </p>
-            </form>
-            <SocialBar />
+            <div className={styles.overlay}>
+              <h1>Get in touch</h1>
+              <form
+                name="contact"
+                method="post"
+                action="/thanks/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={this.handleSubmit}
+              >
+                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                <input type="hidden" name="form-name" value="contact" />
+                <p hidden>
+                  <label>
+                    Don’t fill this out:{" "}
+                    <input name="bot-field" onChange={this.handleChange} />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    Your name:
+                    <br />
+                    <input
+                      type="text"
+                      name="name"
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    Your email:
+                    <br />
+                    <input
+                      type="email"
+                      name="email"
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    Message:
+                    <br />
+                    <textarea name="message" onChange={this.handleChange} />
+                  </label>
+                </p>
+                <p>
+                  <button type="submit">Send</button>
+                </p>
+              </form>
+              <SocialBar />
+            </div>
           </div>
         </BackgroundSection>
       </Layout>
