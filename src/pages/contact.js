41bd2e4b85a5d4bde396ded4584904c632b22617
David Bernhard <davidbernhard@live.com>
2019-03-20 14:49:04 +0000

    background image component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,9 @@
 import React from "react"
-import Layout from "../components/layout"
 import { navigateTo } from "gatsby-link"
+
+import Layout from "../components/layout"
+import BackgroundSection from "../components/contactBackground"
+import styles from "./contact.module.scss"
 
 function encode(data) {
   return Object.keys(data)
@@ -36,50 +39,56 @@ export default class Contact extends React.Component {
   render() {
     return (
       <Layout>
-        <div>
-          <h1>Contact</h1>
-          <form
-            name="contact"
-            method="post"
-            action="/thanks/"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-            onSubmit={this.handleSubmit}
-          >
-            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-            <input type="hidden" name="form-name" value="contact" />
-            <p hidden>
-              <label>
-                Don’t fill this out:{" "}
-                <input name="bot-field" onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your name:
-                <br />
-                <input type="text" name="name" onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your email:
-                <br />
-                <input type="email" name="email" onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <label>
-                Message:
-                <br />
-                <textarea name="message" onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
-          </form>
-        </div>
+        <BackgroundSection>
+          <div className={styles.formContainer}>
+            <h1>Get in touch</h1>
+            <form
+              name="contact"
+              method="post"
+              action="/thanks/"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              onSubmit={this.handleSubmit}
+            >
+              {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+              <input type="hidden" name="form-name" value="contact" />
+              <p hidden>
+                <label>
+                  Don’t fill this out:{" "}
+                  <input name="bot-field" onChange={this.handleChange} />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your name:
+                  <br />
+                  <input type="text" name="name" onChange={this.handleChange} />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your email:
+                  <br />
+                  <input
+                    type="email"
+                    name="email"
+                    onChange={this.handleChange}
+                  />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Message:
+                  <br />
+                  <textarea name="message" onChange={this.handleChange} />
+                </label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
+          </div>
+        </BackgroundSection>
       </Layout>
     )
   }
