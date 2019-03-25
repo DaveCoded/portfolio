d14c02f794fdc8cb6852431e0b4d56539c86424d
David Bernhard <davidbernhard@live.com>
2019-03-25 16:22:48 +0000

    normal gatsby-image on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { navigateTo } from "gatsby-link"
+import Img from "gatsby-image"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SocialBar from "../components/SocialBar/social-bar"
-// import BackgroundSection from "../components/contactBackground"
 import styles from "./contact.module.scss"
 
 function encode(data) {
@@ -40,7 +41,10 @@ export default class Contact extends React.Component {
   render() {
     return (
       <Layout>
-        {/* <BackgroundSection> */}
+        <Img
+          fluid={this.props.data.bubbles.childImageSharp.fluid}
+          className={styles.background}
+        />
         <div className={styles.formContainer}>
           <div className={styles.overlay}>
             <h1>Get in touch</h1>
@@ -97,8 +101,19 @@ export default class Contact extends React.Component {
             <SocialBar />
           </div>
         </div>
-        {/* </BackgroundSection> */}
       </Layout>
     )
   }
 }
+
+export const query = graphql`
+  query {
+    bubbles: file(relativePath: { eq: "make-magic.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
