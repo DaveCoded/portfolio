ba127aa01f563817578d149a3670bd6bb417bee5
David Bernhard <davidbernhard@live.com>
2019-03-26 12:51:09 +0000

    changed to smaller image

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -108,7 +108,7 @@ export default class Contact extends React.Component {
 
 export const query = graphql`
   query {
-    bubbles: file(relativePath: { eq: "make-magic.jpg" }) {
+    bubbles: file(relativePath: { eq: "mountain-jump.jpg" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid
