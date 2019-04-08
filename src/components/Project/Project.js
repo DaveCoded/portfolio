ce899bc8f38f852a79c5f2628378c5994f1e9bda
David Bernhard <davidbernhard@live.com>
2019-04-08 15:47:51 +0100

    better gradients

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,15 +2,12 @@ import React from "react"
 
 import Link from "gatsby-link"
 import Img from "gatsby-image"
-// import styles from "./Project.module.scss"
+import styles from "./Project.module.scss"
 
 const Project = ({ node }) => (
   <Link to={`projects/${node.slug}`}>
-    <div>
-      <Img
-        fluid={node.featuredImage.fluid}
-        style={{ borderRadius: "0.6rem" }}
-      />
+    <div className={styles.container}>
+      <Img fluid={node.featuredImage.fluid} style={{ borderRadius: "5px" }} />
     </div>
   </Link>
 )
