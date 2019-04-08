66de2ba6f00e3ddbc1568ee8839ce54e34d31cff
David Bernhard <davidbernhard@live.com>
2019-04-08 18:45:47 +0100

    Interests component text

--- src/components/AboutComponents/Interests/Interests.js
+++ src/components/AboutComponents/Interests/Interests.js
@@ -1,5 +1,24 @@
 import React from "react"
 
-const Interests = () => <p>These are my interests; aren't they interesting?</p>
+const Interests = () => {
+  return (
+    <div>
+      <p>
+        I'm interested in far too many things, so end up keeping various hobbies
+        on an <em>ad hoc</em> rotation. I always have a good book on the go, and
+        something beyond my level to learn on the piano.
+      </p>
+      <p>
+        At the moment, other than all the time I dedicate to coding, I am
+        starting two podcasts, one with friends and one bizarre solo project
+        that may or may not ever be published.
+      </p>
+      <p>
+        Otherwise I'm currently improving my drawing and Illustrator and
+        Photoshop skills, playing chess online and learning card tricks.
+      </p>
+    </div>
+  )
+}
 
 export default Interests
