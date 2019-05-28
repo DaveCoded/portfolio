47bf4b826c717591c040e490e733dadc2589bb88
David Bernhard <davidbernhard@live.com>
2019-05-28 19:37:11 +0100

    contact background, layout, socialBar

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { navigateTo } from "gatsby-link"
+import { graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 
 import SocialBar from "../components/SocialBar/social-bar"
 import styles from "./contact.module.scss"
@@ -37,64 +39,82 @@ export default class Contact extends React.Component {
 
   render() {
     return (
-      <div className={styles.Background}>
-        <form
-          name="contact"
-          method="post"
-          action="/thanks/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={this.handleSubmit}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out:{" "}
-              <input name="bot-field" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p className={styles.Input}>
-            <input
-              type="text"
-              name="name"
-              placeholder="Your name"
-              onChange={this.handleChange}
-              required
-            />
-            <label className="visually-hidden" htmlFor="name">
-              Your name
-            </label>
-          </p>
-          <p className={styles.Input}>
-            <input
-              type="email"
-              name="email"
-              placeholder="Email address"
-              onChange={this.handleChange}
-              required
-            />
-            <label className="visually-hidden" htmlFor="email">
-              Your email
-            </label>
-          </p>
-          <p className={styles.Textarea}>
-            <textarea
-              name="message"
-              placeholder="Message"
-              onChange={this.handleChange}
-              required
-            />
-            <label className="visually-hidden" htmlFor-="message">
-              Message
-            </label>
-          </p>
-          <p className={styles.buttonP}>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+      <BackgroundImage
+        className={styles.Background}
+        Tag="div"
+        fluid={this.props.data.forest.childImageSharp.fluid}
+      >
+        <div className={styles.Overlay}>
+          <form
+            name="contact"
+            method="post"
+            action="/thanks/"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={this.handleSubmit}
+          >
+            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+            <input type="hidden" name="form-name" value="contact" />
+            <p hidden>
+              <label>
+                Don’t fill this out:{" "}
+                <input name="bot-field" onChange={this.handleChange} />
+              </label>
+            </p>
+            <p className={styles.Input}>
+              <input
+                type="text"
+                name="name"
+                placeholder="Your name"
+                onChange={this.handleChange}
+                required
+              />
+              <label className="visually-hidden" htmlFor="name">
+                Your name
+              </label>
+            </p>
+            <p className={styles.Input}>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email address"
+                onChange={this.handleChange}
+                required
+              />
+              <label className="visually-hidden" htmlFor="email">
+                Your email
+              </label>
+            </p>
+            <p className={styles.Textarea}>
+              <textarea
+                name="message"
+                placeholder="Message"
+                onChange={this.handleChange}
+                required
+              />
+              <label className="visually-hidden" htmlFor-="message">
+                Message
+              </label>
+            </p>
+            <p className={styles.buttonP}>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+        </div>
         <SocialBar />
-      </div>
+      </BackgroundImage>
     )
   }
 }
+
+export const query = graphql`
+  query {
+    forest: file(relativePath: { eq: "forest-1800.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
