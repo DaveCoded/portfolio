22a9bf91bab864bf98d850523f8c99499eb80798
David Bernhard <davidbernhard@live.com>
2019-05-28 18:13:34 +0100

    remove CoderList profile

--- src/components/SocialBar/social-bar.js
+++ src/components/SocialBar/social-bar.js
@@ -5,12 +5,10 @@ import SocialButton from "./social-button"
 import github from "../../assets/svg/github-button.svg"
 import linkedin from "../../assets/svg/linkedin-button.svg"
 import twitter from "../../assets/svg/twitter-button.svg"
-import coderlist from "../../assets/svg/coderlist-button.svg"
 
 const githubURL = "https://github.com/DaveCoded"
 const linkedinURL = "https://www.linkedin.com/in/dave-bernhard-93b0b876/"
 const twitterURL = "https://twitter.com/daveforall"
-const coderlistURL = "https://www.coderlist.io/profile/coderlist423"
 
 const SocialBar = () => (
   <div className={styles.socialBar}>
@@ -18,7 +16,6 @@ const SocialBar = () => (
       <SocialButton href={githubURL} src={github} alt="Github" />
       <SocialButton href={linkedinURL} src={linkedin} alt="LinkedIn" />
       <SocialButton href={twitterURL} src={twitter} alt="Twitter" />
-      <SocialButton href={coderlistURL} src={coderlist} alt="CoderList" />
     </div>
   </div>
 )
