ba81cc34bfac778fa7f50f567a281d2b928d1e13
David Bernhard <davidbernhard@live.com>
2019-05-31 22:46:27 +0100

    first animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,14 @@ import styles from "./index.module.scss"
 const HomePage = () => {
   return (
     <div className={styles.Background}>
-      <p>
-        <span className={styles.Hello}>Hello. I'm Dave,</span>{" "}
-        <span className={styles.Developer}>
-          a front-end developer from London
-        </span>
+      <p style={{ position: "relative" }}>
+        <span style={{ display: "block", fontSize: "4rem" }} id="hello">
+          Hello. I'm Dave,
+        </span>{" "}
+        <span id="developer">a front-end developer from London</span>
       </p>
       <h1>Welcome to my portfolio</h1>
-      <p className={styles.Explore}>Now go explore...</p>
+      <p style={{ float: "right" }}>Now go explore...</p>
     </div>
   )
 }
