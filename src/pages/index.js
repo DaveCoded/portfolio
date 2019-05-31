def7b5d1e55eb5c7f384cc3fea39e15f94c6de9b
David Bernhard <davidbernhard@live.com>
2019-05-31 22:16:33 +0100

    simpler homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,20 @@
-import React, { Component } from "react"
-import { graphql } from "gatsby"
+import React from "react"
 
-import SocialBar from "../components/SocialBar/social-bar"
 import styles from "./index.module.scss"
 
-import purpleButton from "../assets/svg/purple-button.svg"
-
-class HomePage extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      textClass: null,
-    }
-  }
-
-  render() {
-    return (
-      <main className={styles.Background}>
-        <button className={styles.Button}>
-          <img src={purpleButton} alt="Click Me!" />
-          <span>Click Me!</span>
-        </button>
-        <SocialBar />
-      </main>
-    )
-  }
-
-  // Only animate the h2 on first page load
-  /*
-  componentDidMount() {
-    if (window.sessionStorage.getItem("firstLoadDone") === null) {
-      this.setState({
-        textClass: styles.animate,
-      })
-      window.sessionStorage.setItem("firstLoadDone", 1)
-    } else {
-      this.setState({
-        textClass: styles.loaded,
-      })
-    }
-  } */
+const HomePage = () => {
+  return (
+    <div className={styles.Background}>
+      <p>
+        <span className={styles.Hello}>Hello. I'm Dave,</span>{" "}
+        <span className={styles.Developer}>
+          a front-end developer from London
+        </span>
+      </p>
+      <h1>Welcome to my portfolio</h1>
+      <p className={styles.Explore}>Now go explore...</p>
+    </div>
+  )
 }
 
 export default HomePage
-
-export const query = graphql`
-  query {
-    background: file(relativePath: { eq: "backpng.png" }) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
-`
