a6517d1bfa53ee2e066ec6b99c45f134c5e4baa5
David Bernhard <davidbernhard@live.com>
2019-06-01 16:27:20 +0100

    restrict homepage animations to first load

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,45 +4,60 @@ import SocialBar from "../components/SocialBar/social-bar"
 import styles from "./index.module.scss"
 
 class HomePage extends Component {
-  // constructor() {
-  //   super()
-  //   state: {
-  //   }
-  // }
+  constructor() {
+    super()
+    this.state = {
+      animate: true,
+    }
+  }
 
   render() {
     return (
       <div className={styles.Background}>
         <p style={{ position: "relative" }}>
-          <span style={{ display: "block", fontSize: "4rem" }} id="hello">
+          <span
+            className={this.state.animate ? "fade-in-hello" : null}
+            style={{ display: "block", fontSize: "4rem" }}
+          >
             Hello. I'm Dave,
           </span>{" "}
-          <span id="developer">a front-end developer from London</span>
+          <span
+            className={this.state.animate ? "descend-developer" : null}
+            id="developer"
+          >
+            a front-end developer from London
+          </span>
         </p>
-        <h1 id="welcome">
+        <h1
+          className={this.state.animate ? "fade-in-welcome" : null}
+          id="welcome"
+        >
           Welcome to my
           <br /> portfolio
         </h1>
-        <p style={{ float: "right" }} id="explore">
+        <p
+          className={this.state.animate ? "slide-in-explore" : null}
+          style={{ float: "right" }}
+        >
           Now go explore...
         </p>
-        <SocialBar />
+        <SocialBar animate={this.state.animate} />
       </div>
     )
   }
 
-  // componentDidMount(){
-  //   if (window.sessionStorage.getItem("firstLoadDone") === null) {
-  //     this.setState({
-
-  //     })
-  //     window.sessionStorage.setItem("firstLoadDone", 1)
-  //   } else {
-  //     this.setState({
-  //       textClass: styles.loaded,
-  //     })
-  //   }
-  // }
+  componentDidMount() {
+    if (window.sessionStorage.getItem("firstLoadDone") === null) {
+      this.setState({
+        animate: true,
+      })
+      window.sessionStorage.setItem("firstLoadDone", 1)
+    } else {
+      this.setState({
+        animate: false,
+      })
+    }
+  }
 }
 
 export default HomePage
