176c1953e4c3f30b675bcc5906e87e81f9823589
David Bernhard <davidbernhard@live.com>
2019-06-01 16:12:58 +0100

    convert index to class component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,48 @@
-import React from "react"
+import React, { Component } from "react"
 
 import SocialBar from "../components/SocialBar/social-bar"
 import styles from "./index.module.scss"
 
-const HomePage = () => {
-  return (
-    <div className={styles.Background}>
-      <p style={{ position: "relative" }}>
-        <span style={{ display: "block", fontSize: "4rem" }} id="hello">
-          Hello. I'm Dave,
-        </span>{" "}
-        <span id="developer">a front-end developer from London</span>
-      </p>
-      <h1 id="welcome">
-        Welcome to my
-        <br /> portfolio
-      </h1>
-      <p style={{ float: "right" }} id="explore">
-        Now go explore...
-      </p>
-      <SocialBar />
-    </div>
-  )
+class HomePage extends Component {
+  // constructor() {
+  //   super()
+  //   state: {
+  //   }
+  // }
+
+  render() {
+    return (
+      <div className={styles.Background}>
+        <p style={{ position: "relative" }}>
+          <span style={{ display: "block", fontSize: "4rem" }} id="hello">
+            Hello. I'm Dave,
+          </span>{" "}
+          <span id="developer">a front-end developer from London</span>
+        </p>
+        <h1 id="welcome">
+          Welcome to my
+          <br /> portfolio
+        </h1>
+        <p style={{ float: "right" }} id="explore">
+          Now go explore...
+        </p>
+        <SocialBar />
+      </div>
+    )
+  }
+
+  // componentDidMount(){
+  //   if (window.sessionStorage.getItem("firstLoadDone") === null) {
+  //     this.setState({
+
+  //     })
+  //     window.sessionStorage.setItem("firstLoadDone", 1)
+  //   } else {
+  //     this.setState({
+  //       textClass: styles.loaded,
+  //     })
+  //   }
+  // }
 }
 
 export default HomePage
-
-/*
-if (window.sessionStorage.getItem("firstLoadDone") === null) {
-  this.setState({
-    textClass: styles.animate,
-  })
-  window.sessionStorage.setItem("firstLoadDone", 1)
-} else {
-  this.setState({
-    textClass: styles.loaded,
-  })
-}
-*/
