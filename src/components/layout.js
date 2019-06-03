7010c51570d9c15ddfb3b647c24ef558b5d2bd62
David Bernhard <davidbernhard@live.com>
2019-06-03 21:21:36 +0100

    restructure lifecycle order

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,19 +15,6 @@ class Layout extends Component {
     }
   }
 
-  componentDidMount() {
-    var prevScrollpos = window.pageYOffset
-    window.onscroll = function() {
-      var currentScrollPos = window.pageYOffset
-      if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0"
-      } else {
-        document.getElementById("navbar").style.top = "-80px"
-      }
-      prevScrollpos = currentScrollPos
-    }
-  }
-
   toggleNav() {
     this.setState(prevState => {
       return {
@@ -79,6 +66,19 @@ class Layout extends Component {
         />
       </>
     )
+  }
+
+  componentDidMount() {
+    var prevScrollpos = window.pageYOffset
+    window.onscroll = function() {
+      var currentScrollPos = window.pageYOffset
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0"
+      } else {
+        document.getElementById("navbar").style.top = "-80px"
+      }
+      prevScrollpos = currentScrollPos
+    }
   }
 }
 
