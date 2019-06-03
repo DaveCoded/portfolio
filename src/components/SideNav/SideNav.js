ded5df050c79f49e3463406a124ad0bb643025cb
David Bernhard <davidbernhard@live.com>
2019-06-03 18:43:33 +0100

    style links

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,19 +1,31 @@
 import React from "react"
+import { Link } from "gatsby"
 
+import cv from "../../assets/documents/CV.pdf"
 import styles from "./SideNav.module.scss"
 
 const SideNav = () => {
   return (
     <nav className={styles.SideNav}>
-      <button aria-label="Close navigation sidebar" class="close-nav">
+      <button aria-label="Close navigation sidebar" className={styles.CloseNav}>
         &times;
       </button>
       <ul>
-        <li>about</li>
-        <li>projects</li>
-        <li>blog</li>
-        <li>cv</li>
-        <li>contact</li>
+        <li>
+          <Link to="/about">about</Link>
+        </li>
+        <li>
+          <Link to="/projects">projects</Link>
+        </li>
+        <li>
+          <Link to="/blog">blog</Link>
+        </li>
+        <li>
+          <a href={cv}>cv</a>
+        </li>
+        <li>
+          <Link to="/contact">contact</Link>
+        </li>
       </ul>
     </nav>
   )
