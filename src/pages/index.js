e5f60a650fe5fb6845893a5c017e4fa0bccdc476
David Bernhard <davidbernhard@live.com>
2019-06-06 22:49:22 +0100

    center index text with flexbox

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,36 +16,38 @@ class HomePage extends Component {
     return (
       <Layout animate={this.state.animate}>
         <div className={styles.Background}>
-          <p style={{ position: "relative" }} id="hello-p">
-            <span
+          <div style={{ width: "77%" }}>
+            <p style={{ position: "relative" }} id="hello-p">
+              <span
+                className={
+                  this.state.animate ? "fade-in-hello hello-span" : "hello-span"
+                }
+              >
+                Hello. I'm Dave,
+              </span>{" "}
+              <br />
+              <span
+                className={this.state.animate ? "descend-developer" : null}
+                id="developer"
+              >
+                a front-end developer from London
+              </span>
+            </p>
+            <h1
+              className={this.state.animate ? "fade-in-welcome" : null}
+              id="welcome"
+            >
+              Welcome to my
+              <br /> portfolio
+            </h1>
+            <p
               className={
-                this.state.animate ? "fade-in-hello hello-span" : "hello-span"
+                this.state.animate ? "slide-in-explore explore-p" : "explore-p"
               }
             >
-              Hello. I'm Dave,
-            </span>{" "}
-            <br />
-            <span
-              className={this.state.animate ? "descend-developer" : null}
-              id="developer"
-            >
-              a front-end developer from London
-            </span>
-          </p>
-          <h1
-            className={this.state.animate ? "fade-in-welcome" : null}
-            id="welcome"
-          >
-            Welcome to my
-            <br /> portfolio
-          </h1>
-          <p
-            className={
-              this.state.animate ? "slide-in-explore explore-p" : "explore-p"
-            }
-          >
-            Now go explore...
-          </p>
+              Now go explore...
+            </p>
+          </div>
           <SocialBar animate={this.state.animate} />
         </div>
       </Layout>
