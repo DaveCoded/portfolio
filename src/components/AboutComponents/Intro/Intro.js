9bed3aa34252dbd809772f50bc281d0f0f23f070
David Bernhard <davidbernhard@live.com>
2019-06-06 16:27:14 +0100

    intro responsive

--- src/components/AboutComponents/Intro/Intro.js
+++ src/components/AboutComponents/Intro/Intro.js
@@ -5,20 +5,20 @@ import { graphql, StaticQuery } from "gatsby"
 import styles from "./Intro.module.scss"
 
 const Intro = () => (
-  <div className={styles.wrapper}>
-    <div className={styles.text}>
-      <p>
-        Hi. Thank you for coming to check out my portfolio. It leverages the
-        awesome power of Gatsby, which lets me write React code, while you enjoy
-        a blazing fast experience. Finally, the best of both worlds!
-      </p>
-      <p>
-        I'm one of the rare few who is interested both in the traditional,
-        presentational aspects of front-end development, as well as the more
-        recent JavaScript takeover. As long as it means a wonderful web to use
-        and work in, I'll give it a go. I also enjoy tinkering with Adobe
-        Illustrator and Photoshop.
-      </p>
+  <div className={styles.grid}>
+    <p className={styles.para1}>
+      Hi. Thank you for coming to check out my portfolio. It leverages the
+      awesome power of Gatsby, which lets me write React code, while you enjoy a
+      blazing fast experience. Finally, the best of both worlds!
+    </p>
+    <p className={styles.para2}>
+      I'm one of the rare few who is interested both in the traditional,
+      presentational aspects of front-end development, as well as the more
+      recent JavaScript takeover. As long as it means a wonderful web to use and
+      work in, I'll give it a go. I also enjoy tinkering with Adobe Illustrator
+      and Photoshop.
+    </p>
+    <div className={styles.currentlyLearning}>
       <h4>Currently learning about:</h4>
       <p>SVG animations and morphing, Redux, and JavaScript design patterns</p>
     </div>
