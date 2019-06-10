9fca4868f6249a8c14ad213b648633c36ab45fb8
David Bernhard <davidbernhard@live.com>
2019-06-10 22:19:44 +0100

    adjust font size etc.

--- src/components/AboutComponents/Interests/Goodreads.js
+++ src/components/AboutComponents/Interests/Goodreads.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 
+import styles from "./Goodreads.module.scss"
+
 const GoodReads = () => (
   <StaticQuery
     query={graphql`
@@ -22,14 +24,16 @@ const GoodReads = () => (
     render={data => {
       const { book } = data.goodreadsShelf.reviews[0]
       return (
-        <div>
-          <h3>Currently reading</h3>
-          <p>
-            <a href={book.link} target="_blank" rel="noopener noreferrer">
-              {book.title}
-            </a>{" "}
-            by {book.authors[0].name}
-          </p>
+        <div className={styles.flexWrapper}>
+          <div>
+            <h3>Currently reading</h3>
+            <p>
+              <a href={book.link} target="_blank" rel="noopener noreferrer">
+                {book.title}
+              </a>{" "}
+              by {book.authors[0].name}
+            </p>
+          </div>
           <a href={book.link} target="_blank" rel="noopener noreferrer">
             <img src={book.image_url} alt="Currently reading" />
           </a>
